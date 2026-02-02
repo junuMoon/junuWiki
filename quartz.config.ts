ca73c4eefe6bad3f6cf39ce751a73bc5bc855064
Junu Moon(Fran) <francomoon7@gmail.com>
2026-02-02 10:45:38 +0900

    Configure baseUrl and pageTitle for junuWiki

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "junuWiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    analytics: null,
+    locale: "ko-KR",
+    baseUrl: "junumoon.github.io/junuWiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
